d473eee3a4a0562384a5d12dee7949a9b92a5e4a
chrischriscris <18-10892@usb.ve>
2024-04-18 23:11:18 -0400

    read toml config and some prototyping

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,11 @@
-use std::env;
+use std::{
+    env, fs,
+    path::{Path, PathBuf},
+};
+use toml_edit::DocumentMut;
 
+/// Supported platforms for Alacritty, along with an ordered list of possible
+/// configuration file locations.
 #[derive(Debug)]
 enum SupportedPlatform {
     Unix,
@@ -14,8 +20,46 @@ fn detect_platform() -> Result<SupportedPlatform, String> {
     };
 }
 
+/// Opens the Alacritty configuration file for the given platform and returns
+/// a string with its content
+fn get_config_file_content(platform: SupportedPlatform) -> Result<String, String> {
+    let mut config_files: Vec<PathBuf> = Vec::new();
+
+    if let SupportedPlatform::Unix = platform {
+        // $XDG_CONFIG_HOME/alacritty/alacritty.toml
+        // $XDG_CONFIG_HOME/alacritty.toml
+        // $HOME/.config/alacritty/alacritty.toml
+        // $HOME/.alacritty.toml
+        let _ = env::var("XDG_CONFIG_HOME").inspect(|val| {
+            config_files.push(PathBuf::from(val.to_owned() + "/alacritty/alacritty.toml"));
+            config_files.push(PathBuf::from(val.to_owned() + "/alacritty.toml"));
+        });
+
+        let _ = env::var("HOME").inspect(|val| {
+            config_files.push(PathBuf::from(
+                val.to_owned() + "/.config/alacritty/alacritty.toml",
+            ));
+            config_files.push(PathBuf::from(val.to_owned() + "/.alacritty.toml"));
+        });
+    } else if let SupportedPlatform::Windows = platform {
+        // %APPDATA$\alacritty\alacritty.toml
+        let _ = env::var("APPDATA").inspect(|val| {
+            config_files.push(PathBuf::from(
+                val.to_owned() + "\\alacritty\\alacritty.toml",
+            ));
+        });
+    }
+
+    for config_file in config_files {
+        if Path::exists(&config_file) {
+            return fs::read_to_string(config_file).map_err(|e| e.to_string());
+        }
+    }
+
+    Err(String::from("Could not find configuration file"))
+}
+
 fn main() {
-    // 1. Detect the OS
     let platform = match detect_platform() {
         Ok(value) => value,
         Err(error) => {
@@ -24,9 +68,23 @@ fn main() {
         }
     };
 
-    // 2. Find configuration file for the platform
+    let config = match get_config_file_content(platform) {
+        Ok(content) => content,
+        Err(error) => {
+            eprintln!("Error: {}", error);
+            std::process::exit(1);
+        }
+    };
 
-    //  2.1 If file doesn't exists, create one
+    // Open the file and read it to a string
+    let mut parsed = config.parse::<DocumentMut>().expect("Invalid config file");
+
+    let imports = parsed["import"].as_array_mut().expect("Not an array ahaha");
+
+    imports.push("~/path/to/a/theme");
+
+    println!("{}", parsed);
+
     //  2.2 If it's not a toml return, only operate on toml
 
     // 3. Read the file, is it possible to keep it open?
@@ -35,7 +93,7 @@ fn main() {
 
     // 4. Display a nice looking UI that lists all the available themes,
     //    lets you scroll through them, preview the colors and fuzzyfind some
-    println!("Select a theme: {:?}", platform);
+    //println!("Select a theme: {:?}", platform);
 
     // 5. Return when escaping
 }
